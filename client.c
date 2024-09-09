501e3690385d1edb73f613bcf6862803e06659b9
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-09-10 00:17:16 +0700

    init commit

--- client.c
+++ client.c
@@ -8,34 +8,34 @@
 int main()
 {
 	int status, valread, client_fd;
-    struct sockaddr_in serv_addr;
-    char buffer[1024] = { 0 };
+    	struct sockaddr_in serv_addr;
+    	char buffer[1024] = { 0 };
 
-    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
-        return -1;
-    }
+    	if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        	printf("\n Socket creation error \n");
+        	return -1;
+    	}
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+    	serv_addr.sin_family = AF_INET;
+    	serv_addr.sin_port = htons(PORT);
 
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
+    	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        	printf("\nInvalid address/ Address not supported \n");
+        	return -1;
+    	}
 
-    if ((status = connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
+    	if ((status = connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
+        	printf("\nConnection Failed \n");
+       		return -1;
+    	}
 
 	while (1) {
 
-    	valread = read(client_fd, buffer, 1024 - 1); 
-    	printf("%s\n", buffer);
+    		valread = read(client_fd, buffer, 1024 - 1); 
+    		printf("%s\n", buffer);
 	}
 
-    close(client_fd);
+   	 close(client_fd);
 	
-    return 0;
+   	 return 0;
 }
